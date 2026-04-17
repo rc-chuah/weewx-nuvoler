4cdfc0cdc149b0b36631de3ab54d412b9933ea2a
RC Chuah <44928288+rc-chuah@users.noreply.github.com>
2026-04-17 17:12:45 +0800

    Update install.py

--- install.py
+++ install.py
@@ -1,1 +1,3 @@
-
+# installer for nuvoler
+# Copyright © 2026 RC Chuah (Based on weewx-windy by Matthew Wall and Jacques Terrettaz)
+# Distributed under the terms of the GNU General Public License (GPLv3)
