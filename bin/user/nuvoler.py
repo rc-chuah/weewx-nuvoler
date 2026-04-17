4123a17d3a613bb46b867183b35a1ca5d794f0b0
RC Chuah <44928288+rc-chuah@users.noreply.github.com>
2026-04-17 17:11:24 +0800

    Update nuvoler.py

--- bin/user/nuvoler.py
+++ bin/user/nuvoler.py
@@ -1,1 +1,3 @@
-
+# weewx extension for nuvoler
+# Copyright © 2026 RC Chuah (Based on weewx-windy by Matthew Wall and Jacques Terrettaz)
+# Distributed under the terms of the GNU General Public License (GPLv3)
